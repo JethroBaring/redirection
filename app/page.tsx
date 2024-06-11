e7a4b68cc276e31ac61a224133a279894bdf4a36
Jethro Baring <baring.jeth@gmail.com>
2024-06-11 14:24:45 +0800

    testing HSTS

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       Testing if it can be put inside an iframe
+      <Link href={'http://tesdasdasdt.com'}>Click me</Link>
     </div>
+    
   );
 }
